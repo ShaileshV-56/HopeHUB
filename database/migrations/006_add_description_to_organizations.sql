5aaad026856228897b6f063c7a0def1bd5c1c139
ShaileshV-56 <shaileshveerapathiran333@gmail.com>
2025-10-26 23:02:35 +0530

    fix: Correct migration file for description column

--- database/migrations/006_add_description_to_organizations.sql
+++ database/migrations/006_add_description_to_organizations.sql
@@ -1,2 +1,4 @@
-﻿-- Add description column to helper_organizations
+﻿
+-- Add description column to helper_organizations
 ALTER TABLE helper_organizations ADD COLUMN IF NOT EXISTS description TEXT;
+
